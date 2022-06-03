f564d28af368c2726af0c5ddaa1a7aebc7e63095
jijicodes <sjieun11@gmail.com>
2022-06-03 15:15:09 -0600

    added react router paths and some color change

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -1,7 +1,17 @@
 import React, { useContext } from "react";
 import "./LandingPage.css";
-import { Reactjs } from "grommet-icons";
-import { Box, Text, ResponsiveContext, Paragraph, Anchor } from "grommet";
+import { Reactjs, Github, Linkedin, Compliance } from "grommet-icons";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  Avatar,
+  Box,
+  Text,
+  ResponsiveContext,
+  Paragraph,
+  Anchor,
+  Nav,
+} from "grommet";
+
 export const LandingPage = () => {
   const size = React.useContext(ResponsiveContext);
   return (
@@ -11,24 +21,63 @@ export const LandingPage = () => {
       ) : (
         <Box
           className="greetingMsg"
-          justify="center"
-          align="center"
           background="rgba(236, 228, 224,0.5)"
+          justify="between"
+          align="center"
           fill
         >
-          <Box pad="small">
+          <Box>
+            <Nav direction="row" pad="medium">
+              <Anchor href="me" label="me" hoverIndicator color="#524127" />
+              <Anchor
+                href="portfolio"
+                label="portfolio"
+                hoverIndicator
+                color="#524127"
+              />
+              <Anchor
+                href="resume"
+                label="resume"
+                hoverIndicator
+                color="#524127"
+              />
+            </Nav>
+          </Box>
+
+          <Box pad="small" gap="small">
             <Text className="typeWriter" size="45px">
               Hello, I'm Jieun Seo.
             </Text>
+            <Box
+              pad="small"
+              gap="small"
+              direction="row"
+              align="center"
+              justify="center"
+            >
+              <Compliance color="black" />
+              <Anchor color="black">frontend</Anchor>
+              <Anchor color="black">developer</Anchor>
+              <Anchor color="black">reactJS</Anchor>
+            </Box>
+            <Text size="19px" color="#524127" weight="bold">
+              I used to teach little kids in pre-school. <br></br> Now I am on
+              my journey of becoming a better programmer everyday.<br></br>{" "}
+            </Text>
+
+            <Box direction="row" justify="center" align="center" gap="medium">
+              <Anchor href="https://github.com/jijicodes" target="_blank">
+                <Github color="#423c22" size="30px" />
+              </Anchor>
+              <Anchor
+                href="https://www.linkedin.com/in/jennacodes/"
+                target="_blank"
+              >
+                <Linkedin color="#423c22" size="27px" />
+              </Anchor>
+            </Box>
           </Box>
-          <Text size="19px">
-            I used to teach little kids in pre-school. <br></br> Now I am on my
-            journey of becoming a better programmer everyday.<br></br>
-            <Anchor>#frontend </Anchor>
-            <Anchor>#developer</Anchor>
-            <Anchor>#reactJS</Anchor>
-            <Anchor> #denver</Anchor>
-          </Text>
+          <Box></Box>
         </Box>
       )}
     </Box>
