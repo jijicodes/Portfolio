5e30193e197c0d170f4ae86e8b7ac73f2decffee
jijicodes <sjieun11@gmail.com>
2022-06-15 15:24:33 -0600

    added responsive design q

--- src/Components/Me/Me.jsx
+++ src/Components/Me/Me.jsx
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import "./Me.css";
 import { Mail } from "grommet-icons";
 import copy from "copy-to-clipboard";
-import { Box, Button, Paragraph, Image, Text, Anchor } from "grommet";
+import {
+  Box,
+  Button,
+  Paragraph,
+  Image,
+  Text,
+  Anchor,
+  ResponsiveContext,
+} from "grommet";
 
 export const Me = () => {
+  const size = useContext(ResponsiveContext);
+  const isSmall = size === "small";
   const [copied, setCopied] = useState("copy");
   return (
     <Box background="#FBFBDE">
@@ -13,13 +23,17 @@ export const Me = () => {
           About Me
         </Box>
         <Box
-          direction="row"
+          direction={isSmall ? "column" : "row"}
           pad="small"
           gap="large"
           justify="center"
           margin="none"
         >
-          <Box width="30%" height="50%">
+          <Box
+            alignSelf={isSmall ? "center" : "stretch"}
+            width={isSmall ? "60vw " : "30%"}
+            height={isSmall ? "50vh " : "50%"}
+          >
             <Image
               fit="cover"
               src={`${process.env.PUBLIC_URL}/Images/seoul.jpg`}
@@ -28,9 +42,9 @@ export const Me = () => {
               ⬆️ Picture of Seoul, South Korea
             </Text>
           </Box>
-          <Box direction="column">
+          <Box direction="column" pad={isSmall ? "small" : "none"}>
             <Paragraph margin="none">
-              <Text weight="bold">
+              <Text weight="bold" size={isSmall ? "small" : "medium"}>
                 Hi👋 my name is <b>Jieun Seo</b> (aka Jenna). <br />I used to
                 teach kids in pre-school 🍎👩‍🏫
                 <br /> Now I am currently 2 years into my coding journey
@@ -39,7 +53,7 @@ export const Me = () => {
               </Text>
             </Paragraph>
             <Paragraph>
-              <Text weight="bold">
+              <Text weight="bold" size={isSmall ? "small" : "medium"}>
                 I have experience creating apps mainly using JavaScript and
                 React but also TypeScript and using a bunch of different
                 libraries for different things building UIs, routing, and APIs
@@ -50,32 +64,34 @@ export const Me = () => {
               <Text color="#d04764" size="large" weight="bolder">
                 ✨Fun Fact About Me✨
               </Text>
-              <Text weight="bold">
+              <Text weight="bold" size={isSmall ? "small" : "medium"}>
                 <br />I moved to the US 🇺🇸 at the age of 16 without knowing any
                 English and eventually got a bachelors degree from UNLV in 2019
-                👩🏻‍🎓. <br />
+                👩🏻‍🎓 <br />
                 Then I started studying programming, now I build React apps
                 daily 💻 📱
                 <br /> Also, I speak English 🇺🇸 - Korean 🇰🇷 - and Japanese 🇯🇵
               </Text>
             </Paragraph>{" "}
-            <Box direction="column" align="center">
-              <Box direction="row" align="center" gap="small">
-                <Mail />
-                <Anchor color="#524127" href="mailto:sjieun11@gmail.com">
-                  sjieun11@gmail.com
-                </Anchor>
-                <Button
-                  primary
-                  color="#524127"
-                  size="small"
-                  label={copied}
-                  onClick={() => {
-                    setCopied(() => "copied!");
-                    copy("sjieun11@gmail.com");
-                  }}
-                />
-              </Box>
+            <Box direction="row" justify="center" align="center" gap="small">
+              <Mail size={isSmall ? "small" : "medium"} />
+              <Anchor
+                size={isSmall ? "small" : "medium"}
+                color="#524127"
+                href="mailto:sjieun11@gmail.com"
+              >
+                sjieun11@gmail.com
+              </Anchor>
+              <Button
+                primary
+                color="#524127"
+                size={isSmall ? "small" : "medium"}
+                label={copied}
+                onClick={() => {
+                  setCopied(() => "copied!");
+                  copy("sjieun11@gmail.com");
+                }}
+              />
             </Box>
           </Box>
         </Box>
