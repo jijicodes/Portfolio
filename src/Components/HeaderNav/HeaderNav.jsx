bab29128a6f0eae6c9ca08bab24f011c97e91627
jijicodes <sjieun11@gmail.com>
2022-06-15 13:27:57 -0600

    added react router links

--- src/Components/HeaderNav/HeaderNav.jsx
+++ src/Components/HeaderNav/HeaderNav.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { Anchor, Nav } from "grommet";
+import { Link } from "react-router-dom";
 
 export const HeaderNav = () => {
   return (
-    <Nav direction="row" pad="medium" justify="center">
-      <Anchor color="#524127" href="/" label="home" />
-      <Anchor color="#524127" href="/me" label="me" />
-      <Anchor color="#524127" href="/portfolio" label="portfolio" />
-      <Anchor color="#524127" href="/resume" label="resume" />
+    <Nav height="5vh" direction="row" pad="medium" justify="center">
+      <Anchor as={Link} color="#524127" href="#" to="/" label="home" />
+      <Anchor as={Link} color="#524127" href="#" to="/me" label="me" />
+      <Anchor
+        as={Link}
+        color="#524127"
+        href="#"
+        to="/portfolio"
+        label="portfolio"
+      />
+      <Anchor as={Link} color="#524127" href="#" to="/resume" label="resume" />
     </Nav>
   );
 };
